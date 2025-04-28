4232e346942db3c2f189c76c0987c39d2832a9d5
alexlimfl <alexlimfl23@gmail.com>
2025-04-29 01:53:50 +0800

    Initial commit. Constructing sidebar, navbar, table with minimum features,

---
+++ README.md
@@ -0,0 +1,3 @@
+# Static Webpage - Patient Management
+
+This project is a simple static website design of a patient managment system. It's coded using HTML, CSS, and JS with additional CSS tools.
---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Sidebar toggle
+const resizeButton = document.querySelector("[data-resize-button]");
+
+resizeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.body.classList.toggle("sb-expanded");
+});
+
+// Responsive sidebar
+/*
+    Desktop (1200px+)
+    Full layout
+
+    Tablet (768–1199px)
+    Sidebar collapsible, horizontal scroll
+
+    Mobile (<768px)
+    Icons-only sidebar, responsive table
+*/
+
+let body = document.body;
+
+function updateBody() {
+    if (window.innerWidth >= 1200) {
+        body.classList.add("sb-expanded");
+    } else body.classList.remove("sb-expanded");
+}
+
+window.addEventListener("resize", updateBody);
